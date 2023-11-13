d352adcc1fb8a2d54dbddaed57a5928fd0dca55d
Borregonorrea <samuel.orregoc@upb.edu.co>
2023-11-12 20:02:21 -0500

    Por fin funciona esto - Navegacion entre paginas

--- app/src/main/java/com/example/proyectotic/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/proyectotic/ui/home/HomeFragment.kt
@@ -4,39 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.proyectotic.R
 import com.example.proyectotic.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val btn_navegar = root.findViewById<Button>(R.id.btn_navegar)
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        btn_navegar.setOnClickListener { findNavController().navigate(R.id.action_navigation_home_to_entretenimiento)}
+
         return root
+
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+
 }
