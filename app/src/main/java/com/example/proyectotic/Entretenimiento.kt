f3a1fbb2bd5a87f869e0474d45b092f8b11ca5bc
Borregonorrea <samuel.orregoc@upb.edu.co>
2023-11-14 05:04:19 -0500

    Lo intente muchachos

--- app/src/main/java/com/example/proyectotic/Entretenimiento.kt
+++ app/src/main/java/com/example/proyectotic/Entretenimiento.kt
@@ -10,6 +10,7 @@ class Entretenimiento : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_entretenimiento, container, false)
     }
 
